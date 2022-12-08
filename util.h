87657e01a5ad603ed3eb0f9261da6eec0eda6213
Kamillo Ferry <kamillo.ferry@outlook.com>
2022-12-09 00:53:50 +0100

    Add some comments

--- util.h
+++ util.h
@@ -12,15 +12,12 @@ using WORD = uint16_t;
 using DWORD = uint32_t;
 using QWORD = uint64_t;
 
+/*
+    Reading little-endian bytes in a host-agnostic way.
+*/
+
 inline WORD getWordLE(const unsigned char* bytes) {
     return ((WORD)bytes[1] << 8) | ((WORD)bytes[0]);
-}
-
-inline std::array<char, sizeof(WORD)> putWordLE(WORD bytes) {
-    return {
-        (char)(bytes & 0xFF),
-        (char)((bytes & 0xFF00) >> 8)
-    };
 }
 
 inline DWORD getDWordLE(const unsigned char* bytes) {
@@ -28,15 +25,6 @@ inline DWORD getDWordLE(const unsigned char* bytes) {
            | ((DWORD)bytes[2] << 16) 
            | ((DWORD)bytes[1] << 8) 
            | ((DWORD)bytes[0]);
-}
-
-inline std::array<char, sizeof(DWORD)> putDWordLE(DWORD bytes) {
-    return {
-        (char)(bytes & 0xFF),
-        (char)((bytes & 0xFF00) >> 8),
-        (char)((bytes & 0xFF0000) >> 16),
-        (char)((bytes & 0xFF000000) >> 24)
-    };
 }
 
 inline QWORD getQWordLE(const unsigned char* bytes) {
@@ -50,6 +38,29 @@ inline QWORD getQWordLE(const unsigned char* bytes) {
            | ((QWORD)bytes[0]);
 }
 
+/*
+    Writing bytes as little-endian in an host-agnostic way.
+*/
+
+inline std::array<char, sizeof(WORD)> putWordLE(WORD bytes) {
+    return {
+        (char)(bytes & 0xFF),
+        (char)((bytes & 0xFF00) >> 8)
+    };
+}
+
+inline std::array<char, sizeof(DWORD)> putDWordLE(DWORD bytes) {
+    return {
+        (char)(bytes & 0xFF),
+        (char)((bytes & 0xFF00) >> 8),
+        (char)((bytes & 0xFF0000) >> 16),
+        (char)((bytes & 0xFF000000) >> 24)
+    };
+}
+
+/*
+    This append the contents of some container types into a std::vector.
+*/
 template<typename T>
 inline void appendVectorToVector(std::vector<T>& to, std::vector<T> from) {
     to.insert(to.end(), from.cbegin(), from.cend());
@@ -60,6 +71,9 @@ inline void appendArrayToVector(std::vector<T>& to, std::array<T, size> from) {
     to.insert(to.end(), from.cbegin(), from.cend());
 }
 
+/*
+    Prints the content of the specified byte buffer, as hex bytes.
+*/
 void printByteBuffer(const char* buffer, size_t n);
 void printByteBuffer(const std::vector<char> buffer);
 
